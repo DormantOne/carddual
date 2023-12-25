43795f218188196987d18c0bd64ae5a335d24000
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-25 17:05:00 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -10,38 +10,39 @@ const firebaseConfig = {
     messagingSenderId: "280023498180",
     appId: "1:280023498180:web:940612b32d85e5a08c7891",
     measurementId: "G-KF6XZ6F2MS"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+let playerAdded = false; // Flag to track if the current player has been added
+
 document.getElementById('signInForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var playerName = document.getElementById('playerName').value;
     addPlayerToFirebase(playerName);
+    playerAdded = true; // Set flag when player is added
 });
 
 function addPlayerToFirebase(name) {
-    // Write to Firebase
     const playerRef = ref(database, 'players/');
     const newPlayerRef = push(playerRef);
-    set(newPlayerRef, { name: name });
+    set(newPlayerRef, { name: name }).then(() => {
+        startListeningToPlayerChanges(); // Start listening to Firebase after player is added
+    });
 }
 
-// Listen for changes in the players list
-onValue(ref(database, 'players/'), function(snapshot) {
-    updatePlayerList(snapshot.val());
-});
-
-
+function startListeningToPlayerChanges() {
+    onValue(ref(database, 'players/'), function(snapshot) {
+        updatePlayerList(snapshot.val());
+    });
+}
 
 function updatePlayerList(players) {
-    // Clear existing list
     var playerList = document.getElementById('playerList');
     playerList.innerHTML = '';
 
-    // Add each player to the list
     for (var key in players) {
         if (players.hasOwnProperty(key)) {
             var li = document.createElement('li');
@@ -50,9 +51,10 @@ function updatePlayerList(players) {
         }
     }
 
-    // Check player count and show the waiting area
+    if (playerAdded) {
+        showWaitingArea();
+    }
     checkPlayerCount();
-    showWaitingArea();
 }
 
 function checkPlayerCount() {
@@ -62,9 +64,7 @@ function checkPlayerCount() {
     }
 }
 
-// Show waiting area and hide sign-in form
 function showWaitingArea() {
     document.getElementById('waitingArea').style.display = 'block';
     document.getElementById('signInArea').style.display = 'none';
 }
-
