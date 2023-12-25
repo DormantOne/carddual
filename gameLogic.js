2945711aafb8a620ae39da7d58c02a4c1a67bb4d
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-25 16:11:42 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,15 +1,46 @@
+// Initialize Firebase (use your own config here)
+firebase.initializeApp(firebaseConfig);
+
+// Reference to your database
+var database = firebase.database();
+
 document.getElementById('signInForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var playerName = document.getElementById('playerName').value;
-    addPlayerToList(playerName);
-    checkPlayerCount();
-    showWaitingArea(); // Call this function to show the waiting area
+    addPlayerToFirebase(playerName);
 });
 
-function addPlayerToList(name) {
-    var li = document.createElement('li');
-    li.textContent = name;
-    document.getElementById('playerList').appendChild(li);
+function addPlayerToFirebase(name) {
+    // Write to Firebase
+    var playerRef = database.ref('players/');
+    var newPlayerRef = playerRef.push();
+    newPlayerRef.set({ name: name });
+
+    // No need to call showWaitingArea here, it will be handled in updatePlayerList
+}
+
+// Listen for changes in the players list
+database.ref('players/').on('value', function(snapshot) {
+    updatePlayerList(snapshot.val());
+});
+
+function updatePlayerList(players) {
+    // Clear existing list
+    var playerList = document.getElementById('playerList');
+    playerList.innerHTML = '';
+
+    // Add each player to the list
+    for (var key in players) {
+        if (players.hasOwnProperty(key)) {
+            var li = document.createElement('li');
+            li.textContent = players[key].name;
+            playerList.appendChild(li);
+        }
+    }
+
+    // Check player count and show the waiting area
+    checkPlayerCount();
+    showWaitingArea();
 }
 
 function checkPlayerCount() {
