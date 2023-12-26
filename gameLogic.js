a3608bdda95ed927940523331de6aff2403f75a5
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-26 12:52:51 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,6 +1,6 @@
 // Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, push, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 // Firebase configuration
 // Firebase configuration
