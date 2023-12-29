141573f55129f2280a1be18cdef17f6b3febb827
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-28 22:10:17 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,8 +1,6 @@
-// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Firebase configuration
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDwsdgIZ1hpEmsQg7sZY0A2vEo71jyhwbY",
@@ -14,94 +12,50 @@ const firebaseConfig = {
     measurementId: "G-KF6XZ6F2MS"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-function addPlayerToFirebase(name) {
-    const playerRef = ref(database, 'players/');
-    const newPlayerRef = push(playerRef);
-    set(newPlayerRef, { name: name });
-}
-
-function startListeningToPlayerChanges() {
-    onValue(ref(database, 'players/'), (snapshot) => {
-        const players = snapshot.val();
-        updatePlayerList(players);
-    });
-}
-
-
-
-function clearPlayersFromFirebase() {
-    const playersRef = ref(database, 'players/');
-    remove(playersRef); // Remove all players from Firebase
-}
-
-// ... [Previous code remains unchanged]
-
-function updatePlayerList(players) {
-    const playerList = document.getElementById('playerList');
-    playerList.innerHTML = ''; // Clear existing list
-
-    for (const key in players) {
-        if (players.hasOwnProperty(key)) {
-            const listItem = document.createElement('li');
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.id = key;
-            checkbox.addEventListener('change', handleCheckboxChange);
-
-            listItem.appendChild(checkbox);
-            listItem.appendChild(document.createTextNode(players[key].name));
-            playerList.appendChild(listItem);
-        }
-    }
-}
-
-let selectedPlayers = new Set(); // To keep track of selected players
-
-function handleCheckboxChange(event) {
-    const playerId = event.target.id;
-    if (event.target.checked) {
-        selectedPlayers.add(playerId);
-        if (selectedPlayers.size > 4) {
-            event.target.checked = false;
-            selectedPlayers.delete(playerId);
-            alert("Only 4 players can be selected!");
-        }
-    } else {
-        selectedPlayers.delete(playerId);
-    }
-    document.getElementById('enterGame').disabled = selectedPlayers.size !== 4;
-}
-
-// ... rest of the script remains the same
-
-// Set up event listeners
 document.addEventListener('DOMContentLoaded', () => {
-    startListeningToPlayerChanges();
+    const playerName = localStorage.getItem('playerName');
+    const playerTeam = localStorage.getItem('team');
 
-    const signInForm = document.getElementById('signInForm');
-    signInForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const playerName = document.getElementById('playerName').value.trim();
-        if (playerName) {
-            addPlayerToFirebase(playerName);
-            document.getElementById('playerName').value = ''; // Clear the input field
-        }
+    if (!playerName || !playerTeam) {
+        alert("Please join a team first.");
+        window.location.href = 'index.html'; // Redirect to team selection page
+        return;
+    }
+
+    document.getElementById('lockInCards').addEventListener('click', () => {
+        const card1 = document.getElementById('card1').value;
+        const card2 = document.getElementById('card2').value;
+        lockInCards(playerName, playerTeam, [card1, card2]);
     });
 
-const clearPlayersButton = document.getElementById('clearPlayers');
-clearPlayersButton.addEventListener('click', () => {
-    clearPlayersFromFirebase();
-    selectedPlayers.clear(); // Add this line to clear the selected players set
-    document.getElementById('enterGame').disabled = true; // Optionally disable the 'Enter Game' button
+    startListeningForTeamChoices(playerTeam);
 });
 
-const enterGameButton = document.getElementById('enterGame');
-enterGameButton.addEventListener('click', () => {
-    window.location.href = 'game.html'; // Redirect to the game page
-});
+function lockInCards(playerName, team, cards) {
+    set(ref(database, `game/${team}/${playerName}`), { cards: cards, locked: true })
+        .then(() => alert("Your cards have been locked in."))
+        .catch((error) => console.error('Error locking in cards:', error));
+}
 
-});
+function startListeningForTeamChoices(playerTeam) {
+    onValue(ref(database, `game/${playerTeam}`), (snapshot) => {
+        const teamChoices = snapshot.val() || {};
+        updateTeamChoicesDisplay(playerTeam, teamChoices);
+    });
+}
+
+function updateTeamChoicesDisplay(teamName, teamChoices) {
+    const teamDiv = document.getElementById(`${teamName}Choices`);
+    teamDiv.innerHTML = '';
+
+    Object.entries(teamChoices).forEach(([player, {cards, locked}]) => {
+        const playerChoices = document.createElement('div');
+        playerChoices.textContent = `${player}: ${cards.join(', ')} ${locked ? '(Locked)' : ''}`;
+        teamDiv.appendChild(playerChoices);
+    });
+}
