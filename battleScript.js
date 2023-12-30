bd13c75018565924d6cbd40925cd44794f68a823
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-30 13:09:32 -0500

    Update battleScript.js

--- battleScript.js
+++ battleScript.js
@@ -1,27 +1,37 @@
-// Assuming Firebase is already initialized
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+// Firebase configuration
+const firebaseConfig = {
+    // Your Firebase configuration
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 document.addEventListener('DOMContentLoaded', () => {
-    const database = getDatabase();
-    listenForReset(database);
+    listenForReset();
 
     document.getElementById('resetEverything').addEventListener('click', () => {
-        triggerReset(database);
+        triggerReset();
     });
 });
 
-function triggerReset(database) {
+function triggerReset() {
     // Clear a specific node in Firebase to signal a reset
     set(ref(database, 'resetSignal'), {})
         .then(() => console.log("Reset signal triggered in Firebase."))
         .catch((error) => console.error("Error triggering reset signal: ", error));
 }
 
-function listenForReset(database) {
+function listenForReset() {
     const resetRef = ref(database, 'resetSignal');
     onValue(resetRef, (snapshot) => {
         if (snapshot.exists() && Object.keys(snapshot.val()).length === 0) {
             // Detected an empty set, perform reset actions
+            console.log("Reset detected, performing reset actions.");
             localStorage.clear();
             window.location.href = 'index.html'; // Redirect to the initial setup page
         }
