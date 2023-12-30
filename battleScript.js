7a1251c3c5d4bccc0a589df21eebdf0967480cfa
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-12-30 13:33:44 -0500

    Update battleScript.js

--- battleScript.js
+++ battleScript.js
@@ -1,5 +1,5 @@
-// Import Firebase modules
-import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// Import necessary Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 // Firebase configuration
@@ -12,37 +12,34 @@ const firebaseConfig = {
     appId: "1:280023498180:web:940612b32d85e5a08c7891",
     measurementId: "G-KF6XZ6F2MS"
 };
+
 // Initialize Firebase only if it hasn't been initialized yet
-if (!getApps().length) {
+if (!firebase.apps.length) {
     initializeApp(firebaseConfig);
 }
+
 const database = getDatabase();
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('resetButton').addEventListener('click', triggerReset);
     listenForReset();
-
-    const resetButton = document.getElementById('resetEverything');
-    if (resetButton) {
-        resetButton.addEventListener('click', () => {
-            triggerReset();
-        });
-    }
 });
 
 function triggerReset() {
-    set(ref(database, 'resetSignal'), {})
-        .then(() => console.log("Reset signal triggered in Firebase."))
-        .catch((error) => console.error("Error triggering reset signal: ", error));
+    // Clear all data in Firebase
+    set(ref(database), {})
+        .then(() => console.log("Firebase data cleared."))
+        .catch((error) => console.error("Error clearing Firebase data: ", error));
 }
 
 function listenForReset() {
-    const resetRef = ref(database, 'resetSignal');
-    onValue(resetRef, (snapshot) => {
-        if (snapshot.exists() && Object.keys(snapshot.val()).length === 0) {
-            console.log("Reset detected, performing reset actions.");
+    // Listen for changes at the root of your Firebase database
+    onValue(ref(database), (snapshot) => {
+        const data = snapshot.val();
+        if (data === null) { // Data is cleared
+            console.log("Reset detected. Clearing local storage and redirecting.");
             localStorage.clear();
-            window.location.href = 'index.html'; // Redirect to the initial setup page
+            window.location.href = 'index.html';
         }
     });
 }
-
